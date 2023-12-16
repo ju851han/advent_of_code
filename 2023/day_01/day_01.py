5bf465927d1bdb21f60746fdf2a90f56294489b9
ju851han <ju851han@web.de>
2023-12-16 16:23:51 +0100

    [ADD] solution for day 1 - part 2

--- 2023/day_01/day_01.py
+++ 2023/day_01/day_01.py
@@ -22,7 +22,7 @@ def get_calibration_value(input_data: str):
 
 
 def _get_calculated_calibration_value(lines: list):
-    """
+    """ Building the sum of numbers per line for the calibration value
     :param lines: list of lines
                   One line corresponds to one element in the lines-list and is one row in the txt-file.
     :return: calibration value
@@ -30,19 +30,51 @@ def _get_calculated_calibration_value(lines: list):
     """
     sum_of_numbers = 0
     for line in lines:
-        numbers_of_current_line = []
-        for sign in line:
-            if sign.isdigit():
-                numbers_of_current_line.append(sign)
-        if numbers_of_current_line:
-            if len(numbers_of_current_line) >= 1:
-                tens = numbers_of_current_line[0]
-                if len(numbers_of_current_line) == 1:
-                    units = tens
-                else:
-                    units = numbers_of_current_line[len(numbers_of_current_line) - 1]
-                sum_of_numbers += int(tens + units)
+        if line:
+            tens = _get_digit(line, True)
+            units = _get_digit(line, False)
+            sum_of_numbers += int(tens + units)
     return sum_of_numbers
+
+
+def _get_digit(line: str, is_tens: bool):
+    """
+    :param line: is one row in the txt-file
+    :param is_tens: flag
+                    If it is True then the searched number is the ten else it is the unit
+    :return: two numbers (tens merged with units) as str
+    :rtype: str
+    """
+    index = 0 if is_tens else len(line) - 1
+
+    while (is_tens and index < len(line)) or (not is_tens and index >= 0):
+        sign = line[index]
+        if sign.isdigit():
+            return sign
+        elif (is_tens and index >= 2) or (not is_tens and (len(line) - index) > 2):
+            part_of_current_read_text = line[0:index + 1] if is_tens else line[index:len(line)]
+            if "one" in part_of_current_read_text:
+                return "1"
+            elif "two" in part_of_current_read_text:
+                return "2"
+            elif "three" in part_of_current_read_text:
+                return "3"
+            elif "four" in part_of_current_read_text:
+                return "4"
+            elif "five" in part_of_current_read_text:
+                return "5"
+            elif "six" in part_of_current_read_text:
+                return "6"
+            elif "seven" in part_of_current_read_text:
+                return "7"
+            elif "eight" in part_of_current_read_text:
+                return "8"
+            elif "nine" in part_of_current_read_text:
+                return "9"
+        if is_tens:
+            index += 1
+        else:
+            index -= 1
 
 
 if __name__ == '__main__':
